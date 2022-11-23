a5a1268e152ed6da4774f7d7fe3aefcab5944b08
orange-hour <swhong0215@gmail.com>
2022-11-23 10:08:42 +0900

    remove unnecessary files

--- django_ETL/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, permissions, generics, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.decorators import api_view
-
-# Create your views here.
-@api_view(['GET']) # 해당 함수 view에서 처리할 http 메소드
-def HelloAPI(request):
-    return Response("hello world!") # http response 형태로 return
