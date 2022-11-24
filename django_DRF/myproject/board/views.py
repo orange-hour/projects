f645087822e7bb0a5c118e920a52ca6d1c4d3aed
orange-hour <swhong0215@gmail.com>
2022-11-24 10:20:48 +0900

    feat: blog success + account app started

--- django_DRF/myproject/board/views.py
+++ django_DRF/myproject/board/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework import generics
 
-# Create your views here.
+# Blog의 목록을 보여주는 역할
+class BlogList(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+# Blog의 detail을 보여주는 역할
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
